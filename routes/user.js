e937efc84cd310e7788f942504a9a7386b81277b
Enes Saraç <202113709044@ogr.balikesir.edu.tr>
2024-05-14 13:55:48 +0300

    .

--- routes/user.js
+++ routes/user.js
@@ -1,84 +1,76 @@
 const express = require("express");
 const router = express.Router();
 
-const db = require("../data/db")
+const db = require("../data/db");
 
-const data = {
-    title: "Popüler Kurslar",
-    categories: ["Web Geliştirme", "Programlama", "Mobil Uygulamalar", "Veri Analizi", "Ofis Uygulamaları"],
-    blogs: [
-        {
-            blogid: 1,
-            baslik: "Komple Uygulamalı Web Geliştirme",
-            aciklama: "Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
-            resim: "1.jpeg",
-            anasayfa: true,
-            onay: true
-        },
-        {
-            blogid: 2,
-            baslik: "Python ile Sıfırdan İleri Seviye Python Programlama",
-            aciklama: "Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
-            resim: "2.jpeg",
-            anasayfa: true,
-            onay: false
-        },
-        {
-            blogid: 3,
-            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
-            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
-            resim: "3.jpeg",
-            anasayfa: false,
-            onay: true
-        },
-        {
-            blogid: 3,
-            baslik: "Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
-            aciklama: "Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
-            resim: "3.jpeg",
-            anasayfa: false,
-            onay: true
-        },
-    ]
-}
+router.use("/blogs/category/:categoryid", async (req, res) => {
+    const id = req.params.categoryid;
 
-router.use("/blogs/:blogid", function(req, res) {
-    res.render("users/blog-details");
-});
+    try {        
+        const [blogs, ] = await db.execute("select * from blog where categoryid = ?", [id]);
+        const [categories, ] = await db.execute("select * from category");
 
-router.use("/blogs", function(req, res) {
-    db.execute("select * from blog where blog.onay = 1")
-        .then(bls => {
-            db.execute("select categoryname from category")
-                .then(cats => {
+        res.render("users/blogs" , {
+            title: categories[id-1].categoryname,
+            blogs: blogs,
+            categories: categories,
+            selectedCategoryId: id
+        })  
 
-                    res.render("users/blogs", {
-                        title: "Tüm Kurslar",
-                        blogs: bls[0],
-                        categories: cats[0]
-                    })
-                })
-                .catch(err => console.log(err));
-        })
-        .catch(err => console.log(err))
+    } catch (err) { console.log(err) }
 
 });
 
-router.use("/", function(req, res) {
-    db.execute("select * from blog where blog.onay = 1 and blog.anasayfa = 1")
-        .then(bls => {
-            db.execute("select categoryname from category")
-                .then(cats => {
-                    
-                    res.render("users/index", {
-                        title: "Popüler Kurslar",
-                        blogs: bls[0],
-                        categories: cats[0]
-                    });
-                })
-                .catch(err => console.log(err))
+router.use("/blogs/:blogid", async (req, res) => {
+    const id = req.params.blogid;
+
+    try {
+        const [blogs, ] = await db.execute("select * from blog where blogid = ?", [id])
+
+        if(blogs[0]){
+            return res.render("users/blog-details", {
+                title: blogs[0].baslik,
+                blog: blogs[0]
+            });
+        }
+        res.redirect("/");
+    
+    } catch (err) { console.log(err) }
+    
+});
+
+router.use("/blogs", async (req, res) => {
+
+    try {
+        const [blogs, ] = await db.execute("select * from blog where blog.onay = 1");
+        const [categories, ] = await db.execute("select * from category");
+
+        res.render("users/blogs", {
+            title: "Tüm Kurslar",
+            blogs: blogs,
+            categories: categories,
+            selectedCategoryId: null
         })
-        .catch(err => console.log(err))
+
+    } catch (err) { console.log(err) }
+
+});
+
+router.use("/", async (req, res) => {
+
+    try {
+        const [blogs, ] = await db.execute("select * from blog where blog.onay = 1 and blog.anasayfa = 1");
+        const [categories, ] = await db.execute("select * from category");
+
+        res.render("users/index", {
+            title: "Popüler Kurslar",
+            blogs: blogs,
+            categories: categories,
+            selectedCategoryId: null
+        })
+
+    } catch (err) { console.log(err) }
+
 });
 
 module.exports = router;
